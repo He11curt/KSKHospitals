c6c5cf87b73f1d3a53ba918d29ba13aee96e1b90
SuJay <118825441+md5-juven@users.noreply.github.com>
2026-01-22 13:07:49 +0530

    Base URL changed

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ import EditMedicine from "./pages/EditMedicine";
 import AdmissionsIPD from "./pages/AdmissionsIPD";
 import ReportForm from "./pages/ReportForm";
 
-axios.defaults.baseURL = "https://kskhospital.prime5d.in/kskhospital/";
+axios.defaults.baseURL = "https://hms.s2jtech.work.gd//kskhospital/";
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
